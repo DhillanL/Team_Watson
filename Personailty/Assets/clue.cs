3a063268780ba83ec07d286c308690558fb4f054
Tom-Porter <tp761@york.ac.uk>
2016-11-21 15:07:15 +0000

    Started clues

---
+++ Personailty/Assets/clue.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class clue{
+	public string name;
+	public room location; // room type
+	public string NPC;
+	public bool is_visiable = false;
+
+	public string getInfo(){
+	}
+}
+
+
---
+++ Personailty/Assets/createClue.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class createClue	{
+
+	public GameObject rope;
+	public GameObject stick;
+	public GameObject clue3;
+	public GameObject clue4;
+	public GameObject clue5;
+	public GameObject clue6;
+	public GameObject clue7;
+	public GameObject clue8;
+	public GameObject clue9;
+
+	public clue clue_rope;
+	public clue clue_stick;
+
+	public Renderer rope_look;
+	public Renderer stick_look;
+
+
+	public createClue (){
+	}
+
+}
+
