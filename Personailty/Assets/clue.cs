bd52d6e17a21e96b9767919418a68d778cea3f58
Tom-Porter <tp761@york.ac.uk>
2016-11-25 14:51:43 +0000

    Updated clues

--- Personailty/Assets/clue.cs
+++ Personailty/Assets/clue.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 
-public class clue{
+public class clue: MonoBehaviour{
 	public string name;
-	//public room location; // room type -- room will be of type scene
-	public string NPC;
+	public string NPC = null;
+	//public scene location; // room type -- room will be of type scene
 	public bool is_visiable = false;
+	public bool is_bloody = false;
+	public string NPC_text = null;
+	//public scene[] spawnable_rooms = new scene[10];
+	public Sprite look;
 
-	public void getInfo(){    // set to voidjust so the 
+	public GameObject Interaction;
+	public GameObject me;
+
+
+	public string getName(){
+		return name;
 	}
-}
+	public void getInfo(){  
+	}
+	public string getNPC(){
+		return NPC;
+	}
+	public void getLocation(){
+		//return location;
+	}
+	//public string getInfo(){
+	//}
+	}
 
 
--- Personailty/Assets/createClue.cs
+++ Personailty/Assets/createClue.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 
-public class createClue	{
+public class createClue: MonoBehaviour{
+
+	public gameManager game_man;
 
 	public GameObject rope;
 	public GameObject stick;
@@ -16,13 +18,85 @@ public class createClue	{
 
 	public clue clue_rope;
 	public clue clue_stick;
+	public clue clue_3;
+	public clue clue_4;
+	public clue clue_5;
+	public clue clue_6;
+	public clue verbal_clue_1;
+	public clue verbal_clue_2;
 
-	public Renderer rope_look;
-	public Renderer stick_look;
+	public SpriteRenderer rope_look;
+	public SpriteRenderer stick_look;
 
+	public GameObject[] gameobjectlst = new GameObject[6];
 
-	public createClue (){
+	public clue[] physical_clue_list = new clue[6];
+	public clue[] verbal_clue_list = new clue[2];
+
+	public void setClues(){
+		clue_rope = rope.GetComponent<clue>();
+		rope_look = rope.GetComponent<SpriteRenderer>();
+
+		clue_stick = stick.GetComponent<clue>();
+		stick_look = stick.GetComponent<SpriteRenderer>();
 	}
 
-}
+	public void make_rope() {
+		clue_rope.name = "Rope";
+		clue_rope.is_visiable = true;
+		clue_rope.is_bloody = true;
+		rope_look.sprite = clue_rope.look;
+	}
 
+	public void make_stick() {
+		clue_rope.name = "Stick";
+		clue_rope.is_visiable = true;
+		clue_rope.is_bloody = true;
+		stick_look.sprite = clue_stick.look;
+	}
+
+	public void make_physical_clue_list(){ //make list of physical clues
+		physical_clue_list [1] = clue_rope;
+		physical_clue_list [2] = clue_stick;
+		physical_clue_list [3] = clue_3;
+		physical_clue_list [4] = clue_4;
+		physical_clue_list [3] = clue_5;
+		physical_clue_list [4] = clue_6;
+	}
+
+	public void make_verbal_clue_list(){ //make list of physical clues
+		verbal_clue_list [1] = verbal_clue_1;
+		verbal_clue_list [2] = verbal_clue_2;
+	}
+
+	public void make_game_object_lst(){
+		gameobjectlst [0] = rope;
+		gameobjectlst [1] = stick;
+		gameobjectlst [2] = clue3;
+		gameobjectlst [3] = clue4;
+		gameobjectlst [4] = clue5;
+		gameobjectlst [5] = clue6;
+		gameobjectlst [6] = clue7;
+		gameobjectlst [7] = clue8;
+	}
+
+	public void put_physical_clues_in_rooms(){
+		game_man.GetComponent<gameManager>().setroom11(gameobjectlst[0]);
+	}
+	public void makeClues(){
+		setClues ();
+
+		make_rope ();
+		make_stick ();
+
+		make_game_object_lst ();
+		make_physical_clue_list ();
+		make_verbal_clue_list ();
+		Debug.Log("clue made");
+
+	}
+	void start(){
+		makeClues ();
+
+	}
+}
