da0798041119d433a49e34ff45976d48ee5fab7b
Dhillan <dhillan_lad@hotmail.com>
2017-01-20 18:08:19 +0000

    Added the first unit test

---
+++ Personailty/Assets/Editor/Engine_Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEditor;
+
+[TestFixture]
+public class Engine_Test
+{
+
+    [Test]
+    public void Crime_Test()
+    {
+        CreateCrime Crime1 = new CreateCrime();
+
+        Crime1.murder7 = "Valid";
+
+        Crime1.Set_Murder(6);
+
+        var value = Crime1.get_murder();
+
+        Debug.Log(value);
+
+        Assert.AreEqual(value,Crime1.murder7);
+    }	// Use this for initialization
+
+}
