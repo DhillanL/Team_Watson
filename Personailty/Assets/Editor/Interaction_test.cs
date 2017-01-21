2a3bc5918e02fb617c14d4122ac09b1bf4354c60
BenaminT <bjt522@york.ac.uk>
2017-01-21 14:59:07 +0000

    small updates

---
+++ Personailty/Assets/Editor/Interaction_test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+[TestFixture]
+public class Interaction_test
+{
+
+    [Test]
+    public void player_personailty()
+    {
+        Personality_player personality = new Personality_player();
+        personality.setintro("testintro");
+
+        Assert.AreEqual(personality.Intro(),"testintro");
+    }
+
+    [Test]
+    public void interaction()
+    {
+        
+
+
+    }
+
+
+
+
+
+}
